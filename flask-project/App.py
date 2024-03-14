80a53ba7d4b5845b22606bb50151ad7ac2cbe88f
Lucas Gomes <lukasgds66@outlook.com>
2024-03-14 17:02:08 -0300

    introduction flask

--- flask-project/App.py
+++ flask-project/App.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, send_file
+from flask import (Flask, render_template,
+                   jsonify)
 
 
 app = Flask(__name__)
+
+
+api_example = {
+    "Name": "Lucas",
+    "Git": "/lucasgommes",
+    "Language": "Python, Java",
+    "Framwork": "Flask, Spring"
+}
 
 
 @app.route('/')
@@ -11,7 +20,7 @@ def home():
 
 @app.route('/show_API', methods=['POST'])
 def show_api():
-    return 'Something'
+    return jsonify(api_example)
 
 
 app.run(debug=True)
